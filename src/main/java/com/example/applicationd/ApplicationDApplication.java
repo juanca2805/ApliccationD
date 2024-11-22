710f3d178ba59e3a4a55bcd657acdf04cc2fb9d2
juanca2805 <juan.pedraza2805@gmail.com>
2024-11-22 14:05:34 -0500

    First commit Create proyect and configure conexion database

---
+++ src/main/java/com/example/applicationd/ApplicationDApplication.java
@@ -0,0 +1,14 @@
+package com.example.applicationd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/applicationd/ApplicationDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
