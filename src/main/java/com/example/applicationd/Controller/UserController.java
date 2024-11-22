e65d5a966bd63f8526203f744a17dbc8e27620f9
juanca2805 <juan.pedraza2805@gmail.com>
2024-11-22 15:08:38 -0500

    Add DeleteMapping

--- src/main/java/com/example/applicationd/Controller/UserController.java
+++ src/main/java/com/example/applicationd/Controller/UserController.java
@@ -3,9 +3,11 @@ package com.example.applicationd.Controller;
 import com.example.applicationd.Entity.User;
 import com.example.applicationd.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,4 +30,19 @@ public class UserController {
     public User getUserById(@PathVariable Long id) {
         return (User) userRepository.findById(id).orElse(null);
     }
-}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if (userOptional.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.ok("Usuario eliminado exitosamente");
+        } else {
+            return ResponseEntity.status(404).body("Usuario no encontrado");
+        }
+    }
+
+
+    }
+
